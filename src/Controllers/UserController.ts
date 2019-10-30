67de074bd469b5aa9dfb399c9046c6cfc4d651b0
Paulo José <paulocardosob@gmail.com>
2019-10-30 08:29:17 -0300

    Implement project Structure

---
+++ src/Controllers/UserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+
+class UserController {
+    public index = async (req: Request, res: Response): Promise<Response> => {
+
+    }
+
+    public create = async (req: Request, res: Response): Promise<Response> => {
+
+    }
+
+    public update = async (req: Request, res: Response): Promise<Response> => {
+
+    }
+
+    public delete = async (req: Request, res: Response): Promise<Response> => {
+
+    }
+}
+
+export default new UserController()
